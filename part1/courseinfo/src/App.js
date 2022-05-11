518520529ace3f709a61a9053baf568fb995c1c2
Oladele Oriyomi <marshall_400@yahoo.com>
2022-05-11 15:53:04 +0300

    Exercise 1.3
    Exercise 1.3: Course information, step 3 using objects in our application.

--- part1/courseinfo/src/App.js
+++ part1/courseinfo/src/App.js
@@ -1,4 +1,5 @@
 const Header = (props) => {
+  console.log(props)
   return (
     <>
       <h1>{props.course} </h1>
@@ -7,6 +8,7 @@ const Header = (props) => {
 }
 
 const Part = (props) => {
+  console.log(props)
   return (
     <>
       <p>{props.part + " " + props.exercises} </p>
@@ -15,6 +17,7 @@ const Part = (props) => {
 }
 
 const Content = (props) => {
+  console.log(props)
   return (
     <>
       <Part part={props.part1} exercises={props.exercises1} />
@@ -25,6 +28,7 @@ const Content = (props) => {
 }
 
 const Total = (props) => {
+  console.log(props)
   return (
     <>
       <p>Number of exercises {props.exercises1 + props.exercises2 + props.exercises3} </p>
@@ -36,18 +40,27 @@ const Total = (props) => {
 
 const App = () => {
   const course = 'Half Stack application development'
-  const part1 = 'Fundamentals of React'
-  const exercises1 = 10
-  const part2 = 'Using props to pass data'
-  const exercises2 = 7
-  const part3 = 'State of a component'
-  const exercises3 = 14
+  const part1 = {
+    name: 'Fundamentals of React',
+    exercises: 10
+  }
+  const part2 = {
+    name: 'Using props to pass data',
+    exercises: 7
+  }
+  const part3 = {
+    name: 'State of a component',
+    exercises: 14
+  }
 
   return (
     <div>
       <Header course={course} />
-      <Content part1={part1} exercises1={exercises1} part2={part2} exercises2={exercises2} part3={part3} exercises3={exercises3} />
-      <Total exercises1={exercises1} exercises2={exercises2} exercises3={exercises3} />
+      <Content part1={part1.name} exercises1={part1.exercises} 
+        part2={part2.name} exercises2={part2.exercises} part3={part3.name} 
+        exercises3={part3.exercises} />
+      <Total exercises1={part1.exercises} exercises2={part2.exercises} 
+        exercises3={part3.exercises} />
     </div>
   )
 }
