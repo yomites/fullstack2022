5827c40116acc4d0d16ae17caa655048fc71a1ec
Oladele Oriyomi <marshall_400@yahoo.com>
2022-05-13 23:33:50 +0300

    Exercise 1.14
    Anecdote step 3.  Addition of the functionality to display the anecdote with the largest number of votes.

--- part1/anecdotes/src/App.js
+++ part1/anecdotes/src/App.js
@@ -4,11 +4,31 @@ const Button = ({ handleClick, text }) => (
   <button onClick={handleClick}><b>{text}</b></button>
 )
 
+const Headings = ({ text }) => <div><h2>{text}</h2></div>
+  
+
 const VotesArray=(vote, index)=> {
   console.log(vote, index)
   const copy = [ ...vote ]
   copy[index] += 1
   return copy
+}
+
+const HighestVoteIndex=(vote)=> {
+  const copy = [ ...vote ]
+  const index=copy.indexOf(Math.max(...copy))
+  return (
+    index
+  ) 
+}
+
+const AnecdoteAndVotesDisplay = (props) => {
+  return (
+    <div>
+      <b>{props.anecdotes}</b><br />
+      <b>has {props.votes} votes </b> <br />
+    </div>
+  )
 }
 
 const App = () => {
@@ -25,7 +45,7 @@ const App = () => {
   const [selected, setSelected] = useState(0)
   const [votes, setVotes] = useState(Array(anecdotes.length).fill(0))
   
-  const texts = ['next anecdote', 'vote']
+  const texts = ['next anecdote', 'vote', 'Anecdote of the day', 'Anecdote with most votes']
 
   const setToSelected = () => setSelected(Math.floor(Math.random() * anecdotes.length) + 0)
   console.log(selected)
@@ -35,10 +55,13 @@ const App = () => {
 
   return (
     <div>
-      <b>{anecdotes[selected]}</b><br />
-      <b>has {votes[selected]} votes </b> <br />
+      <Headings text={texts[2]} />
+      <AnecdoteAndVotesDisplay anecdotes={anecdotes[selected]} votes={votes[selected]} />
       <Button handleClick={handleVoteClick} text={texts[1]} />
       <Button handleClick={setToSelected} text={texts[0]} />
+      <Headings text={texts[3]} />
+      <AnecdoteAndVotesDisplay anecdotes={anecdotes[HighestVoteIndex(votes)]} 
+          votes={votes[HighestVoteIndex(votes)]} />
     </div>
   )
 }
