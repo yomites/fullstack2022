52467a0f7e78c1baf80882527d8196099c482a99
Oladele Oriyomi <marshall_400@yahoo.com>
2022-05-15 01:07:26 +0300

    Exercise 2.1
    Adding Course component to our course information application.

--- part2/courseinfo/src/App.js
+++ part2/courseinfo/src/App.js
@@ -1,47 +1,55 @@
+const Course = ({ course }) => {
+  console.log(course)
+  return (
+    <div>
+      <Header course={course.name} />
+      <Content parts={course.parts} />
+    </div>
+  )
+}
+
 const Header = ({ course }) => <h1>{course}</h1>
 
-const Total = ({ sum }) => <p>Number of exercises {sum}</p>
+// const Total = ({ sum }) => <p>Number of exercises {sum}</p>
 
 const Part = ({ part }) => 
   <p>
-    {part.name} {part.exercises}
+    <b>{part.name} {part.exercises} </b>
   </p>
 
 const Content = ({ parts }) => 
-  <>
-    <Part
-      part={parts[0]} 
-    />
-    <Part
-      part={parts[1]} 
-    />
-    <Part
-      part={parts[2]} 
-    />      
-  </>
+  <div>
+    {parts.map(part => 
+          <Part key={part.id} part={part} />
+    )}
+  </div>
 
 const App = () => {
-  const course = 'Half Stack application development'
-  const parts = [
-    {
-      name: 'Fundamentals of React',
-      exercises: 10
-    },
-    {
-      name: 'Using props to pass data',
-      exercises: 7
-    },
-    {
-      name: 'State of a component',
-      exercises: 14
-    }
-  ]
+  const course = {
+    id: 1,
+    name: 'Half Stack application development',
+    parts: [
+      {
+        name: 'Fundamentals of React',
+        exercises: 10,
+        id: 1
+      },
+      {
+        name: 'Using props to pass data',
+        exercises: 7,
+        id: 2
+      },
+      {
+        name: 'State of a component',
+        exercises: 14,
+        id: 3
+      }
+    ]
+  }
 
   return (
     <div>
-      <Header course={course} />
-      <Content parts={parts} />
-      <Total sum={parts[0].exercises + parts[1].exercises + parts[2].exercises} />
+      <Course course={course} />
     </div>
   )
 }
