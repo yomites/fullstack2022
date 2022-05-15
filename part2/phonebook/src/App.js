1eee34af79b230f87ced05eeaf5da395d1bde64f
Oladele Oriyomi <marshall_400@yahoo.com>
2022-05-15 23:10:31 +0300

    Exercise 2.9
    Addiition of a search field that can be used to filter the list of people by name.

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -2,21 +2,30 @@ import { useState } from 'react'
 import Person from './components/Person'
 
 const App = () => {
-  const [persons, setPersons] = useState([]) 
+  const [persons, setPersons] = useState([
+    { name: 'Arto Hellas', number: '040-123456', id: 1 },
+    { name: 'Ada Lovelace', number: '39-44-5323523', id: 2 },
+    { name: 'Dan Abramov', number: '12-43-234345', id: 3 },
+    { name: 'Mary Poppendieck', number: '39-23-6423122', id: 4 }
+  ]) 
   const [newName, setNewName] = useState('')
   const [newNumber, setNewNumber] = useState('')
+  const [nameSearch, setNameSearch] = useState('')
+  const [nameToShow, setNameToShow] = useState([])
 
   const addPerson = (event) => {
     event.preventDefault()
     console.log('button clicked', event.target)
     const newPerson = {
-      name: newName, phone: newNumber
+      name: newName, number: newNumber
     }
 
-    if (newPerson.name === '' || newPerson.phone === '') {
+    const duplicate = persons.find(person => person.name.toLowerCase() === newPerson.name.toLowerCase())
+
+    if (newPerson.name === '' || newPerson.number === '') {
       window.alert('Name or phone number can not be empty')
     }
-    else if (persons.find(person => person.name.toLowerCase() === newPerson.name.toLowerCase())) {
+    else if (duplicate !== undefined) {
       window.alert(`${newPerson.name} is already added to phonebook`)
       setNewName('')
       setNewNumber('')
@@ -28,20 +37,30 @@ const App = () => {
     }
   }
 
-  const handleNameChange = (event) => {
+  const handleNameChange = (event) => setNewName(event.target.value)
+  
+  const handleNumberChange = (event) => setNewNumber(event.target.value)
+
+  const handleSearchChange = (event) => {
+    const arr = persons.filter(element =>
+      element.name.toUpperCase().includes
+        (event.target.value.toUpperCase()))
     console.log(event.target.value)
-    setNewName(event.target.value)
+    setNameSearch(event.target.value)
+    setNameToShow(arr)
   }
 
-  const handleNumberChange = (event) => {
-    console.log(event.target.value)
-    setNewNumber(event.target.value)
-  }
+  const personsArray = (nameSearch === '') ? persons : nameToShow
 
   return (
     <div>
       <h2>Phonebook</h2>
-      <form onSubmit={addPerson}>
+      <div>
+          filter shown with <input value={nameSearch}
+          onChange={handleSearchChange}  />
+        </div>
+      <form onSubmit={addPerson}> 
+      <h2>add a new</h2>
         <div>
           name: <input value={newName}
           onChange={handleNameChange} />
@@ -54,7 +73,7 @@ const App = () => {
         </div>
       </form>
       <h2>Numbers</h2>
-      {persons.map(person => 
+      {personsArray.map(person => 
         <Person key={person.name} person={person} />  
       )}
     </div>
--- part2/phonebook/src/components/Person.js
+++ part2/phonebook/src/components/Person.js
@@ -1,3 +1,3 @@
-const Person = ({ person }) => <div>{person.name} {person.phone}</div>
+const Person = ({ person }) => <div>{person.name} {person.number}</div>
 
 export default Person
