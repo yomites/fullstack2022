5f34baae624bbfc60be63e263a5bf8a173bfa94e
Oladele Oriyomi <marshall_400@yahoo.com>
2022-05-17 13:50:44 +0300

    Exercise 2.11
    Storing the initial state of the application in the file db.json.

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -1,20 +1,26 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
+import axios from 'axios'
 import Persons from './components/Persons'
 import PersonForm from './components/PersonForm'
 import Filter from './components/Filter'
 
 
 const App = () => {
-  const [persons, setPersons] = useState([
-    { name: 'Arto Hellas', number: '040-123456', id: 1 },
-    { name: 'Ada Lovelace', number: '39-44-5323523', id: 2 },
-    { name: 'Dan Abramov', number: '12-43-234345', id: 3 },
-    { name: 'Mary Poppendieck', number: '39-23-6423122', id: 4 }
-  ]) 
+  const [persons, setPersons] = useState([]) 
   const [newName, setNewName] = useState('')
   const [newNumber, setNewNumber] = useState('')
   const [nameSearch, setNameSearch] = useState('')
-  const [nameToShow, setNameToShow] = useState([])
+
+  useEffect(() => {
+    console.log('effect')
+    axios
+      .get('http://localhost:3001/persons')
+      .then(response => {
+        console.log('promise fulfilled')
+        setPersons(response.data)
+      })
+  }, [])
+  console.log('render', persons.length, 'persons')
 
   const addPerson = (event) => {
     event.preventDefault()
@@ -45,15 +51,12 @@ const App = () => {
   const handleNumberChange = (event) => setNewNumber(event.target.value)
 
   const handleSearchChange = (event) => {
-    const arr = persons.filter(element =>
-      element.name.toUpperCase().includes
-        (event.target.value.toUpperCase()))
     console.log(event.target.value)
     setNameSearch(event.target.value)
-    setNameToShow(arr)
   }
 
-  const personsArray = (nameSearch === '') ? persons : nameToShow
+  const personsArray = nameSearch ? persons.filter(element => element.name.toUpperCase().includes
+      (nameSearch.toUpperCase())) : persons
 
   return (
     <div>
