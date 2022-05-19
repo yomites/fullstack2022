62ca23371c94bc6bd9e51508aa584dae8d37f8ad
Oladele Oriyomi <marshall_400@yahoo.com>
2022-05-20 00:00:56 +0300

    Applying esling to take care of the warning
    ESlint

--- part2/phonebook/src/services/person.js
+++ part2/phonebook/src/services/person.js
@@ -1,24 +1,24 @@
 import axios from 'axios'
 const baseUrl = 'http://localhost:3001/persons'
 
-const getAll = () => {
+const getAll = async () => {
     const request = axios.get(baseUrl)
     return request.then(response => response.data)
   }
   
-  const create = newObject => {
+  const create = async newObject => {
     const request = axios.post(baseUrl, newObject)
     return request.then(response => response.data)
   }
   
-  const remove = (id) => {
+  const remove = ( id ) => {
     return axios.delete(`${baseUrl}/${id}`)
   }
 
-  const update = (id, newObject) => {
+  const update = async ( id, newObject ) => {
     const request = axios.put(`${baseUrl}/${id}`, newObject)
     return request.then(response => response.data)
   }
 
-  
+  // eslint-disable-next-line import/no-anonymous-default-export
   export default { getAll, create, remove, update }
