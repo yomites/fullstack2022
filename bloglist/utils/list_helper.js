fd00ab922a05a82139a6b06c10bb5606a616cdfc
Oladele Oriyomi <marshall_400@yahoo.com>
2022-05-26 23:29:44 +0300

    Exercise 4.4
    Define a new totalLikes function that receives a list of blog posts as a parameter. The function returns the total sum of likes in all of the blog posts.
    
    Write appropriate tests for the function.

--- bloglist/utils/list_helper.js
+++ bloglist/utils/list_helper.js
@@ -5,4 +5,12 @@ const dummy = (blogs) => {
   }
 }
 
-module.exports = { dummy }
+const totalLikes = (blogs) => {
+  const likesArray = blogs.map(blog => blog.likes)
+  const reducer = (sum, item) => {
+    return sum + item
+  }
+  return likesArray.reduce(reducer, 0)
+}
+
+module.exports = { dummy, totalLikes }
