78ffbb3192d10c1309da7886a261c37c47e985ba
Oladele Oriyomi <marshall_400@yahoo.com>
2022-05-26 21:44:56 +0300

    Exercise 4.2
    Refactor the application into separate modules as shown earlier in this part of the course material.

---
+++ bloglist/controllers/blogs.js
@@ -0,0 +1,32 @@
+const blogsRouter = require('express').Router()
+const Blog = require('../models/blog')
+
+blogsRouter.get('/', (request, response) => {
+  Blog
+    .find({})
+    .then(blogs => {
+      response.json(blogs)
+    })
+})
+
+blogsRouter.post('/', (request, response, next) => {
+  const blog = new Blog(request.body)
+
+  blog
+    .save()
+    .then(result => {
+      response.status(201).json(result)
+    })
+    .catch(error => next(error))
+})
+
+blogsRouter.get('/:id', (request, response, next) => {
+  Blog
+    .findById(request.params.id)
+    .then(blogs => {
+      response.json(blogs)
+    })
+    .catch(error => next(error))
+})
+
+module.exports = blogsRouter
