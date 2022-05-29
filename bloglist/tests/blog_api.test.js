dba79ae372f4c3b27c772692a1ddd16f961e7e33
Oladele Oriyomi <marshall_400@yahoo.com>
2022-05-29 18:49:18 +0300

    Exercise 4.8
    Use the supertest package for writing a test that makes an HTTP GET request to the /api/blogs url. Verify that the blog list application returns the correct amount of blog posts in the JSON format.
    
    Once the test is finished, refactor the route handler to use the async/await syntax instead of promises.

---
+++ bloglist/tests/blog_api.test.js
@@ -0,0 +1,37 @@
+const mongoose = require('mongoose')
+const supertest = require('supertest')
+const helper = require('./test_helper')
+const app = require('../app')
+const api = supertest(app)
+const Blog = require('../models/blog')
+
+beforeEach(async () => {
+  await Blog.deleteMany({})
+  let blogObject = new Blog(helper.initialBlogs[0])
+  await blogObject.save()
+  blogObject = new Blog(helper.initialBlogs[1])
+  await blogObject.save()
+})
+
+describe('HTTP requests', () => {
+  test('all blogs are returned as json', async () => {
+    await api.get('/api/blogs').expect(200).expect('Content-Type', /application\/json/)
+  })
+
+  test('all blogs are returned', async () => {
+    const response = await api.get('/api/blogs')
+
+    expect(response.body).toHaveLength(helper.initialBlogs.length)
+  })
+
+  test('a specicifc blog is within the returned blogs', async () => {
+    const response = await api.get('/api/blogs')
+
+    const title = response.body.map(r => r.title)
+    expect(title).toContain('another note cypress 1')
+  })
+})
+
+afterAll(() => {
+  mongoose.connection.close()
+})
---
+++ bloglist/tests/test_helper.js
@@ -0,0 +1,25 @@
+const Blog = require('../models/blog')
+
+const initialBlogs = [
+  {
+    title: 'another note cypress 1',
+    author: 'Brian Kelly',
+    url: 'http://gogcypress.com',
+    likes: 1,
+    id: '628f5b4d2fa44c8e5a4c1676'
+  },
+  {
+    title: 'The building of another note cypress 2',
+    author: 'Tommy Hilfieger',
+    url: 'http://gogcypress.com',
+    likes: 1,
+    id: '628f671e7acf608b48f4d437'
+  }
+]
+
+const blogsInDb = async () => {
+  const blogs = await Blog.find({})
+  return blogs.map(blog => blog.toJSON())
+}
+
+module.exports = { initialBlogs, blogsInDb }
