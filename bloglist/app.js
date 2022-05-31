dd64ce1388497eb2ae9d7dc39435eb72a559960d
Oladele Oriyomi <marshall_400@yahoo.com>
2022-06-01 00:13:20 +0300

    Exercise 4.20
    This example from part 4 shows taking the token from the header with the getTokenFrom helper function.
    
    If you used the same solution, refactor taking the token to a middleware. The middleware should take the token from the Authorization header and place it to the token field of the request object.

--- bloglist/app.js
+++ bloglist/app.js
@@ -23,6 +23,7 @@ mongoose.connect(config.MONGODB_URI)
 app.use(cors())
 app.use(express.static('build'))
 app.use(express.json())
+app.use(middleware.tokenExtractor)
 app.use(middleware.requestLogger)
 
 app.use('/api/blogs', blogsRouter)
