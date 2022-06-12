cb1b2ae90d62be40361209711fdac70e364a7ce7
Oladele Oriyomi <marshall_400@yahoo.com>
2022-06-12 16:36:16 +0300

    Exercise 5.18
    Make tests for logging in. Test both successful and unsuccessful login attempts. Make a new user in the beforeEach block for the tests.
    
    Optional bonus exercise: Check that the notification shown with unsuccessful login is displayed red.

--- bloglist_frontend/cypress/e2e/blog_app.spec.cy.js
+++ bloglist_frontend/cypress/e2e/blog_app.spec.cy.js
@@ -1,6 +1,12 @@
 describe('Blog app', function() {
   beforeEach(function() {
     cy.request('POST', 'http://localhost:3003/api/testing/reset')
+    const user = {
+      name: 'Matti Luukkainen',
+      username: 'mluukkai',
+      password: 'salainen'
+    }
+    cy.request('POST', 'http://localhost:3003/api/users/', user)
     cy.visit('http://localhost:3000')
   })
 
@@ -9,5 +15,28 @@ describe('Blog app', function() {
     cy.get('#username-input').parent().should('contain', 'username')
     cy.get('#password-input').parent().should('contain', 'password')
     cy.get('#loginButton').should('contain', 'login')
+  })
+
+  describe('Login',function() {
+    it('succeeds with correct credentials', function() {
+      cy.get('#username-input').type('mluukkai')
+      cy.get('#password-input').type('salainen')
+      cy.get('#loginButton').click()
+
+      cy.contains('Matti Luukkainen logged in')
+    })
+
+    it('fails with wrong credentials', function() {
+      cy.get('#username-input').type('mluukkai')
+      cy.get('#password-input').type('wrongpassword')
+      cy.get('#loginButton').click()
+
+      cy.get('.error')
+        .should('contain', 'invalid username or password')
+        .and('have.css', 'color', 'rgb(255, 0, 0)')
+        .and('have.css', 'border-style', 'solid')
+
+      cy.get('html').should('not.contain', 'Matti Luukkainen logged in')
+    })
   })
 })
