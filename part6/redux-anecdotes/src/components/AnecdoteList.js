1c30740378660078692d6fcad317ec2b202467d0
Oladele Oriyomi <marshall_400@yahoo.com>
2022-06-15 22:46:28 +0300

    Exercise 6.8
    Separate the rendering of the anecdote list into its own component called AnecdoteList. Move all logic related to voting for an anecdote to this new component.

---
+++ part6/redux-anecdotes/src/components/AnecdoteList.js
@@ -0,0 +1,32 @@
+import { useDispatch, useSelector } from 'react-redux'
+import { voteFor } from '../reducers/anecdoteReducer'
+
+const Anecdote = ({ anecdote, vote }) => {
+  return (
+    <div>
+      <div>
+        {anecdote.content}
+      </div>
+      <div>
+            has {anecdote.votes}
+        <button onClick={() => vote(anecdote.id)}>vote</button>
+      </div>
+    </div>
+  )
+}
+
+const AnecdoteList = () => {
+  const dispatch = useDispatch()
+  const anecdotes = useSelector(state => state)
+  const sortedAnecdotes = anecdotes.sort((a, b) => b.votes - a.votes)
+
+  return (sortedAnecdotes.map(a =>
+    <Anecdote
+      key={a.id}
+      anecdote={a}
+      vote={() => dispatch(voteFor(a.id))}
+    />
+  ))
+}
+
+export default AnecdoteList
